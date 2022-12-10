04eb2fa1b9e1e74799c292d111e6f0bf83b69319
Siddharth Khatri <siddxharth@gmail.com>
2022-12-11 00:10:01 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Siddharth Khatri on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
